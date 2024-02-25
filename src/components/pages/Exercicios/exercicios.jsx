b3b0b594b34dd5254258d22775c1ab4ff764987e
MarjoriLins <marjori89lins@gmail.com>
2024-02-25 19:37:28 -0300

    Exemplo useMemo.

--- src/components/pages/Exercicios/exercicios.jsx
+++ src/components/pages/Exercicios/exercicios.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState, useRef, useMemo } from "react";
 
 function Exercicios() {
   // Exemplo de useRef
@@ -8,11 +8,27 @@ function Exercicios() {
       paragraphRef.current.innerText += "Novo texto";
     }
   };
+  // Exemplo de useMemo
+
+  const [numero, setNumero] = useState(0);
+
+  const calcularParOuImpar = (num) => {
+    console.log("Calculando...");
+    return num % 2 === 0 ? "par" : "impar";
+  };
+
+  const tipoNumero = useMemo(() => calcularParOuImpar(numero), [numero]);
+
   return (
     <>
       <h1>Exemplo de useRef</h1>
       <p ref={paragraphRef}>Esse é um parágrafo.</p>
       <button onClick={adicionarTexto}>Adicionar texto</button>
+      <h1>Exemplo de useMemo</h1>
+      <p>
+        {numero} é um número {tipoNumero}
+      </p>
+      <button onClick={() => setNumero(numero + 1)}>Incrementar</button>
     </>
   );
 }
