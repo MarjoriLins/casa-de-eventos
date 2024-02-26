e7779efa069ff70ea06b8b60efe26e714cc899ef
MarjoriLins <marjori89lins@gmail.com>
2024-02-26 12:06:16 -0300

    Criado a Página de Adicionar Evento.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/pages/Login/login.jsx";
 import ListaEventos from "./components/pages/ListaEventos/listaEventos.jsx";
+import AdicionarEvento from "./components/pages/AdicionarEvento/adicionarEvento.jsx";
 import Homepage from "./components/pages/Homepage/homepage.jsx";
 import DetalhesEvento from "./components/pages/DetalhesEvento/detalhesEvento.jsx";
 import Contato from "./components/pages/Contato/contato.jsx";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<ListaEventos />} />
+        <Route path="/adicionar-evento" element={<AdicionarEvento />} />
         <Route path="/evento/:id" element={<DetalhesEvento />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/exercicios" element={<Exercicios />} />
---
+++ src/components/pages/AdicionarEvento/adicionarEvento.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import axios from "axios";
+
+const AdicionarEvento = () => {
+  const [evento, setEvento] = useState({
+    titulo: "",
+    data: "",
+    horario: "",
+    preco: 0,
+    url_da_imagem: "",
+    evento_privado: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEvento((prevEvent) => ({
+      ...prevEvent,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/eventos",
+        evento
+      );
+      console.log("Evento adicionado com sucesso", response.data);
+      alert("Evento adicionado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao adicionar evento", error);
+      alert("Erro ao adicionar evento. Por favor, tente novamente.");
+    }
+  };
+
+  return (
+    <>
+      <h1>Adicionar Evento</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Titulo:
+          <input
+            type="text"
+            name="titulo"
+            value={evento.titulo}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Data:
+          <input
+            type="date"
+            name="data"
+            value={evento.data}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Hora:
+          <input
+            type="time"
+            name="horario"
+            value={evento.horario}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Preço:
+          <input
+            type="number"
+            name="preco"
+            value={evento.preco}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          URL da imagem:
+          <input
+            type="text"
+            name="url_da_imagem"
+            value={evento.url_da_imagem}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Evento privado:
+          <input
+            type="checkbox"
+            name="evento_privado"
+            checked={evento.evento_privado}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Adicionar</button>
+      </form>
+    </>
+  );
+};
+
+export default AdicionarEvento;
